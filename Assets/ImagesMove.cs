592ba381ec45a46f4b9d97b38e76f61e15292e3c
JuanKamil123 <140729008+JuanKamil123@users.noreply.github.com>
2023-08-18 16:23:57 +0700

    Add files via upload

---
+++ Assets/ImagesMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImagesMove : MonoBehaviour
+{
+    
+    int[] posX = new int[] { 0, -18, -36, -54 };
+    int idx = 0;
+    public AudioSource[] audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio[idx].Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if (idx < posX.Length - 1)
+            {
+                idx++;
+                audio[idx].Play();
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            if (idx > 0)
+            {
+                idx--;
+                audio[idx].Play();
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position,
+new Vector3(posX[idx], transform.position.y), 50 * Time.deltaTime);
+    }
+}
---
+++ Assets/ScenesManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public string EnterScene;
+    public string EscapeScene;
+    public bool isEscapeForQuit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            Debug.Log("Name Scene: " + EnterScene);
+            SceneManager.LoadScene(EnterScene);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isEscapeForQuit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                Debug.Log("Name Scene: " + EscapeScene);
+                SceneManager.LoadScene(EscapeScene);
+            }
+        }
+    }
+}
+            
+    
+
+
